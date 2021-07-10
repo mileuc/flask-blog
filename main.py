379e942527590c0b718a33282a02fd42df82231f
Michael <michaell7ii@hotmail.com>
2021-07-09 18:36:56 -0600

    Add psycopg2-binary package and database config var

--- main.py
+++ main.py
@@ -12,13 +12,14 @@ from functools import wraps
 from dotenv import load_dotenv
 import os
 
+load_dotenv("./.env")
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
