a9b248a2966454a0f5b25302971cdadfaa9b64fb
Michael <michaell7ii@hotmail.com>
2021-08-07 21:20:15 -0600

    Fix is_edit variable

--- main.py
+++ main.py
@@ -219,7 +219,7 @@ def add_new_post():
         db.session.add(new_post)
         db.session.commit()
         return redirect(url_for("get_all_posts"))
-    return render_template("make-post.html", form=form, logged_in=current_user.is_authenticated)
+    return render_template("make-post.html", form=form, logged_in=current_user.is_authenticated, is_edit=False)
 
 
 @app.route("/edit-post/<int:post_id>")
@@ -242,7 +242,7 @@ def edit_post(post_id):
         db.session.commit()
         return redirect(url_for("show_post", post_id=post.id))
 
-    return render_template("make-post.html", form=edit_form, logged_in=current_user.is_authenticated)
+    return render_template("make-post.html", form=edit_form, logged_in=current_user.is_authenticated, is_edit=True)
 
 
 @app.route("/delete/<int:post_id>")
